941eeb39764abf9de65fb3c51365df71492aa04d
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-11-27 13:32:49 -0300

    entrega exercício 8

--- M1S08/notification-app/src/app/components/notification-list/notification-list.component.ts
+++ M1S08/notification-app/src/app/components/notification-list/notification-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { notifications } from './notifications';
 
 @Component({
   selector: 'ntap-notification-list',
@@ -6,17 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./notification-list.component.scss']
 })
 export class NotificationListComponent {
-  notifications = [{ 
-    'app' : "Ifood",
-    'timestamp' : "há 17 minutos",
-    'description' : "Kelvis chegou com seu pedido."
-  }, {
-    'app' : "Whatsapp",
-    'timestamp' : "há 30 minutos",
-    'description' : "Ageu: hype!"
-  }, {
-    'app' : "Youtube",
-    'timestamp' : "há 2 horas",
-    'description' : "Marombeiro Cientista fez um upload de um novo vídeo."
-  },]
+  notifications = notifications;
 }
---
+++ M1S08/notification-app/src/app/components/notification-list/notifications.ts
@@ -0,0 +1,13 @@
+export const notifications = [{ 
+    'app' : "Ifood",
+    'timestamp' : "há 17 minutos",
+    'description' : "Kelvis chegou com seu pedido."
+  }, {
+    'app' : "Whatsapp",
+    'timestamp' : "há 30 minutos",
+    'description' : "Ageu: hype!"
+  }, {
+    'app' : "Youtube",
+    'timestamp' : "há 2 horas",
+    'description' : "Marombeiro Cientista fez um upload de um novo vídeo."
+  },]
