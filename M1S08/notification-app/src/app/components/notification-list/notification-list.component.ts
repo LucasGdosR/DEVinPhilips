0e492a1c1e07aec191c2aa13a3fd60dbfad5e201
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-11-27 12:51:37 -0300

    entrega exercício 7

--- M1S08/notification-app/src/app/components/notification-list/notification-list.component.ts
+++ M1S08/notification-app/src/app/components/notification-list/notification-list.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./notification-list.component.scss']
 })
 export class NotificationListComponent {
-
+  notifications = [{ 
+    'app' : "Ifood",
+    'timestamp' : "há 17 minutos",
+    'description' : "Kelvis chegou com seu pedido."
+  }, {
+    'app' : "Whatsapp",
+    'timestamp' : "há 30 minutos",
+    'description' : "Ageu: hype!"
+  }, {
+    'app' : "Youtube",
+    'timestamp' : "há 2 horas",
+    'description' : "Marombeiro Cientista fez um upload de um novo vídeo."
+  },]
 }
