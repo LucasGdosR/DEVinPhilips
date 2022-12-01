52c7e9fb97f60c355364dfbae73878736574ec34
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-12-01 08:12:44 -0300

    entrega exercício 6

--- M1S09/ex1a6/src/app/components/item-card/card.component.ts
+++ M1S09/ex1a6/src/app/components/item-card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
@@ -7,9 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class ItemCardComponent {
-  onePunchMan: any = {
-    name: 'Saitama, "Careca de Capa"',
-    urlImg: 'https://criticalhits.com.br/wp-content/uploads/2020/06/saitama-1.jpg',
-    description: 'O "Careca de Capa" é um herói em ascensão, tendo subido rapidamente da classe C, para B, e atualmente A. As pessoas ficam surpresas com seu relacionamento com Genos, o Ciborgue Demoníaco, que o trata com reverência, chamando-o de sensei. Sua proximidade com King, o homem mais forte do mundo, também assusta os outros.',
-  }
+  @Input() name: string = "";
+  @Input() urlImg: string = "";
+  @Input() description: string = "";
 }
