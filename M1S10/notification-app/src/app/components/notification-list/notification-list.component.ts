0fae5157fb9780c6ba68d1759eaa44a2c6a45065
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-12-06 14:34:39 -0300

    entrega exercício 1

---
+++ M1S10/notification-app/src/app/components/notification-list/notification-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { notifications } from './notifications';
+
+@Component({
+  selector: 'ntap-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss']
+})
+export class NotificationListComponent {
+  notifications = notifications;
+  
+  read(notification: { lido: boolean}){
+    notification.lido = true;
+  }
+}
