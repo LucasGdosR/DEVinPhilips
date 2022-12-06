43d18d35d95baf502c0db1ae122a30ce94cf2700
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-12-06 14:44:23 -0300

    entrega exercício 2

--- M1S10/notification-app/src/app/app.module.ts
+++ M1S10/notification-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NotificationListComponent } from './components/notification-list/notification-list.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotificationListComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
