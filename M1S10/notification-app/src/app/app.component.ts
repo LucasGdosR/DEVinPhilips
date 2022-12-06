d10a316720797402562b7aa4d3cbc115d454d35b
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-12-06 15:02:56 -0300

    entrega exercício 3

--- M1S10/notification-app/src/app/app.component.ts
+++ M1S10/notification-app/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notification-app';
+  reroute(readUnreadAll: string){
+    switch (readUnreadAll) {
+      case "all":
+        console.log("Filtrando todos");
+        break;
+      case "read":
+        console.log("Filtrando apenas lidos");
+        break;
+      case "unread":
+        console.log("Filtrando apenas não lidos");
+        break;
+    }
+  }
 }
--- M1S10/notification-app/src/app/components/filter/filter.component.ts
+++ M1S10/notification-app/src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'ntap-filter',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
-
+  @Output() filter: EventEmitter<string> = new EventEmitter();
 }
