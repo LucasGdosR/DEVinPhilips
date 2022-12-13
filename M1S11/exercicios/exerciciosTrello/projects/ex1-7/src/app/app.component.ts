fbe781b58dc0a1df190d72c73fdc25fe59f5f455
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-12-13 10:36:55 -0300

    entrega ex 1

---
+++ M1S11/exercicios/exerciciosTrello/projects/ex1-7/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ex1-7';
+  teste = 'hello world';
+}
