5c5f2b5fbdb6edfee4f5d12c89343d213853e48a
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-02-13 21:27:47 -0300

    entrega exercício 2

--- M2S03/Clinica.java
+++ M2S03/Clinica.java
@@ -10,4 +10,26 @@ public class Clinica {
     Long CNPJ;
     String logradouro;
     Integer codigoDaClinica;
+
+    /*
+    Vamos criar um método para calcular o IMC dos pacientes que veem a Clínica. Crie esse método dentro da classe
+    Clínica e esse método deve retornar o número do IMC e também deve exibir no console qual é a categoria do IMC da
+    pessoa com base na tabela em anexo.
+
+    Esse método deve receber os parâmetros: peso e altura
+
+    Formula IMC = peso / (altura)².
+     */
+    public Double calcularIMC(Double peso, Double altura) {
+        Double imc = peso / Math.pow(altura, 2);
+
+        if (imc < 18.5) System.out.println("Abaixo do peso.");
+        else if (imc < 25) System.out.println("Peso ideal.");
+        else if (imc < 30) System.out.println("Levemente acima do peso.");
+        else if (imc < 35) System.out.println("Obesidade grau 1.");
+        else if (imc < 40) System.out.println("Obesidade grau 2 (severa).");
+        else System.out.println("Obesidade grau 3 (mórbida).");
+
+        return imc;
+    }
 }
