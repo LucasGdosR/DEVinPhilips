3420d5d53de0bd722e5924b9934cf963089de6fc
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-02-13 21:14:44 -0300

    entrega exercício 1

---
+++ M2S03/Clinica.java
@@ -0,0 +1,13 @@
+package DEVinPhilips.M2S03;
+
+public class Clinica {
+    /*
+    Vamos criar um sistema para uma clínica de emagrecimento. Vamos começar pela Classe Clínica. Essa Classe deve ter
+    os atributos nome do dono, nome da clínica, CNPJ, endereço (esse deve ser o logradouro) e o código da Clínica.
+     */
+    String nomeDoDono;
+    String nomeDaClinica;
+    Long CNPJ;
+    String logradouro;
+    Integer codigoDaClinica;
+}
