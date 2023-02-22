9f320646a9b817d0b4889cb826462434c57fb791
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-02-22 15:19:39 -0300

    entrega exercício 1

---
+++ M2S04/Ex1/Conta.java
@@ -0,0 +1,9 @@
+package DEVinPhilips.M2S04.Ex1;
+
+public abstract class Conta {
+    protected double saldo;
+
+    protected double obterSaldoAtual() {
+        return saldo;
+    };
+}
---
+++ M2S04/Ex1/Operavel.java
@@ -0,0 +1,7 @@
+package DEVinPhilips.M2S04.Ex1;
+
+public interface Operavel {
+    void depositar(double valor);
+
+    void sacar(double valor);
+}
