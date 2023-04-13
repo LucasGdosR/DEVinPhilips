0522e9c700e241e370b33feb329e1123eb14de52
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-04-13 08:20:44 -0300

    Entrega ex1.

---
+++ M2S11/main/java/com/example/m2s11/M2S11Application.java
@@ -0,0 +1,13 @@
+package com.example.m2s11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2S11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2S11Application.class, args);
+    }
+
+}
