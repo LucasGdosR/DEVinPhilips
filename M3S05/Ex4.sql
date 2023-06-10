ece332f2ba7fdd4e17ff7890eb283a317d3757c7
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-06-10 19:44:43 -0300

    Entrega Ex4.

---
+++ M3S05/Ex4.sql
@@ -0,0 +1,11 @@
+DECLARE
+	valorTotal NUMBER;
+BEGIN
+	SELECT SUM(Valor) INTO total FROM ProdutoPreco;
+	IF total < 150 THEN
+			DBMS_OUTPUT.PUT_LINE('Soma do produto menor 150.00');
+		ELSIF total < 500 THEN
+			DBMS_OUTPUT.PUT_LINE('Soma do produto está entre 150 e 500');
+		ELSE
+			DBMS_OUTPUT.PUT_LINE('Soma dos produtos está acima de 500,00');
+END;
